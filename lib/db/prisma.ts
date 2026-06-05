195ce36e3ba80cc2340b774f6cfb8cc9f2afcead
Sandro Roldan <roldan.eng.software@gmail.com>
2026-06-05 14:07:55 -0300

    Configure Prisma Postgres adapter

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export function getPrisma() {
   if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient();
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    globalForPrisma.prisma = new PrismaClient({ adapter });
   }
   return globalForPrisma.prisma;
 }
